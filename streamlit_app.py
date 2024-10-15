08af704e973d3533deb19872091b33a592ab3585
Khushbu Sharma <khushbusharma@Khushbus-MacBook-Air.local>
2024-10-15 13:26:45 +0530

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,111 @@
+import streamlit as st
+import numpy as np
+import altair as alt
+import pandas as pd
+from datetime import time, datetime
+
+st.title("Hello World")
+
+# st.header("My header is this, it is given with a divider below", divider="rainbow")
+
+# col1, col2 = st.columns(2)
+
+# with col1:
+#     x = st.slider ("choose an x value", 1, 10)
+# with col2:
+#     st.write ("The value of x is ", x)
+
+# st.header('st.button')
+# if st.button ('Say hello'):
+#     st.write('why hello there')
+# else:
+#     st.write('Goodbye')
+
+# Day 7
+
+# st.header('st.write')
+
+# # Example 1
+
+# st.write('Hello, *World!* :sunglasses:')
+
+# # Example 2
+
+# st.write(1234)
+
+# # Example 3
+
+# df = pd.DataFrame({
+#      'first column': [1, 2, 3, 4],
+#      'second column': [10, 20, 30, 40]
+#      })
+# st.write(df)
+
+# # Example 4
+
+# st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# # Example 5
+
+# df2 = pd.DataFrame(
+#      np.random.randn(200, 3),
+#      columns=['a', 'b', 'c'])
+# c = alt.Chart(df2).mark_circle().encode(
+#      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+# st.write(c)
+
+
+# Day 8
+
+
+# st.header('st.slider applications')
+
+# # Example 1
+
+# st.subheader('Age')
+
+# age = st.slider('How old are you?', 0, 130, 25)
+# st.write("I'm ", age, 'years old')
+
+# # Example 2
+
+# st.subheader('Range slider')
+
+# values = st.slider(
+#      'Select a range of values',
+#      0.0, 100.0, (25.0, 75.0))
+# st.write('Values:', values)
+
+# # Example 3
+
+# st.subheader('Range time slider')
+
+# appointment = st.slider(
+#      "Schedule your appointment:",
+#      value=(time(11, 30), time(12, 45)))
+# st.write("You're scheduled for:", appointment)
+
+# # Example 4
+
+# st.subheader('Datetime slider')
+
+# start_time = st.slider(
+#      "When do you start?",
+#      value=datetime(2020, 1, 1, 9, 30),
+#      format="MM/DD/YY - hh:mm")
+# st.write("Start time:", start_time)
+
+# Day 9
+
+st.header("Line Chart")
+
+chart_data = pd.DataFrame(np.random.randn(20,3),columns= ['a','b','c'])
+st.line_chart(chart_data)
+
+# Day 10
+
+st.header("Selectbox")
+option = st.selectbox("what's your favorite color ?", ("Blue", "White", "Red", "Balck"))
+
+st.write("Your fav color is - ", option)
+
